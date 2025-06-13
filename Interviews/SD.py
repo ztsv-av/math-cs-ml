6cff7da0581d9e418f85d6aa022bb03f712b8d7b
ztsv-av <ztsv.1337@gmail.com>
2025-06-13 09:16:11 +0200

    SD prep

---
+++ Interviews/SD.py
@@ -0,0 +1,6 @@
+# 8. Longest Subarray with Equal Number of 0s and 1s
+# Given a binary array (only 0s and 1s), 
+# find the length of the longest contiguous subarray with equal number of 0s and 1s.
+# Example:
+#   Input: [0, 1, 0, 1, 1, 0]
+#   Output: 6
